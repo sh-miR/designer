93a3151d94a5db7eed1881b8a9284274b8a849aa
Sylwester Brzęczkowski <sylwek1992@gmail.com>
2013-05-17 00:40:42 +0200

    creaty

---
+++ database/shmirdesignercreate.sql
@@ -0,0 +1,18 @@
+CREATE TABLE nowy_backbone (
+    id integer NOT NULL PRIMARY KEY,
+    name varchar(10) NOT NULL,
+    flanks3_s varchar(80) NOT NULL,
+    flanks3_a varchar(80) NOT NULL,
+    flanks5_s varchar(80) NOT NULL,
+    flanks5_a varchar(80) NOT NULL,
+    loop_s varchar(30) NOT NULL,
+    loop_a varchar(30) NOT NULL,
+    miRNA_s varchar(30) NOT NULL,
+    miRNA_a varchar(30) NOT NULL,
+    miRNA_length integer NOT NULL,
+    miRNA_min integer NOT NULL,
+    miRNA_max integer NOT NULL,
+    structure varchar(200) NOT NULL,
+    homogeneity integer NOT NULL,
+    miRBase_link varchar(200) NOT NULL
+);
