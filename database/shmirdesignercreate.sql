d256202e32a75017d9f0072ca9764875f42123a2
Mateusz Flieger <mateuszflieger@gmail.com>
2013-05-29 10:31:18 +0200

    Connect to database after create, insert wit default

--- database/shmirdesignercreate.sql
+++ database/shmirdesignercreate.sql
@@ -1,6 +1,6 @@
-DROP TABLE backbone;
 DROP DATABASE shmird;
 CREATE DATABASE shmird;
+\c shmird;
 
 CREATE TABLE backbone (
     id SERIAL PRIMARY KEY,
@@ -22,7 +22,7 @@ CREATE TABLE backbone (
 );
 
 INSERT INTO backbone VALUES
-    ('miR-30', 'TGCCTACTGCCTCGGACTTCAAGGGGCTACTTTAGGAGCA', 'TGCTCCTAAAGTAGCCCCTTGAAGTCCGAGGCAGTAGGCA',
+    (DEFAULT, 'miR-30', 'TGCCTACTGCCTCGGACTTCAAGGGGCTACTTTAGGAGCA', 'TGCTCCTAAAGTAGCCCCTTGAAGTCCGAGGCAGTAGGCA',
     'CTAAAGAAGGTATATTGCTGTTGACAGTGAGCGAC', 'GTCGCTCACTGTCAACAGCAATATACCTTCTTTAG',
     'cugugaagccacagauggg', 'cccatctgtggcttcacag', 'uguaaacauccucgacuggaag', 'cttccagtcgaggatgtttaca',
     22, 19, 25, '/structures/mir30.rnaml', 2, 'http://www.mirbase.org/cgi-bin/mirna_entry.pl?acc=MI0000088'
