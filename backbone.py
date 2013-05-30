f5ec4b961d2340af039dd2e636a014b466c3db80
Mateusz Flieger <mateuszflieger@gmail.com>
2013-05-30 12:55:12 +0200

    Refactoring

--- backbone.py
+++ backbone.py
@@ -6,6 +6,43 @@ import json
 url = 'http://127.0.0.1:9001/'
 headers = {'content-type': 'application/json'}
 
+class Backbone:
+    def __init__(self, name, flanks3_s, flanks3_a, flanks5_s, flanks5_a, loop_s, loop_a, miRNA_s, miRNA_a, miRNA_length, miRNA_min, miRNA_max, structure, homogeneity, miRBase_link):
+        self.name = name
+        self.flanks3_s = flanks3_s
+        self.flanks3_a = flanks3_a
+        self.flanks5_s = flanks5_s
+        self.flanks5_a = flanks5_a
+        self.loop_s = loop_s
+        self.loop_a = loop_a
+        self.miRNA_s = miRNA_s
+        self.miRNA_a = miRNA_a
+        self.miRNA_length = miRNA_length
+        self.miRNA_min = miRNA_min
+        self.miRNA_max = miRNA_max
+        self.structure = structure
+        self.homogeneity = homogeneity
+        self.miRBase_link = miRBase_link
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'flanks3_s': self.flanks3_s,
+            'flanks3_a': self.flanks3_a,
+            'flanks5_s': self.flanks5_s,
+            'flanks5_a': self.flanks5_a,
+            'loop_s': self.loop_s,
+            'loop_a': self.loop_a,
+            'miRNA_s': self.miRNA_s,
+            'miRNA_a': self.miRNA_a,
+            'miRNA_length': self.miRNA_length,
+            'miRNA_min': self.miRNA_min,
+            'miRNA_max': self.miRNA_max,
+            'structure': self.structure,
+            'homogeneity': self.homogeneity,
+            'miRBase_link': self.miRBase_link
+        }
+
 def qbackbone(method, data=None):
     """
     Acceptable methods(string):
--- database/database.py
+++ database/database.py
@@ -1,5 +1,4 @@
 import psycopg2
-from models import Backbone
 from settings import DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT
 
 def connect():
@@ -38,7 +37,7 @@ def get_by_name(name):
             "miRNA_s, miRNA_a, miRNA_length, miRNA_min, miRNA_max, structure, homogeneity,"\
             "miRBase_link FROM backbone WHERE LOWER(name) = LOWER('%s');" % name
     data = execute_with_one_response(query)
-    return Backbone(*data).serialize() if data else {}
+    return serialize(*data) if data else {}
 
 def get_all():
     query = "SELECT name, flanks3_s, flanks3_a, flanks5_s, flanks5_a, loop_s, loop_a,"\
@@ -55,6 +54,24 @@ def get_multirow_by_query(query):
     data = execute_with_response(query)
     backbones = []
     for row in data:
-        backbones.append(Backbone(*row).serialize())
+        backbones.append(serialize(*row))
     return backbones
 
+def serialize(name, flanks3_s, flanks3_a, flanks5_s, flanks5_a, loop_s, loop_a, miRNA_s, miRNA_a, miRNA_length, miRNA_min, miRNA_max, structure, homogeneity, miRBase_link):
+    return {
+        'name': name,
+        'flanks3_s': flanks3_s,
+        'flanks3_a': flanks3_a,
+        'flanks5_s': flanks5_s,
+        'flanks5_a': flanks5_a,
+        'loop_s': loop_s,
+        'loop_a': loop_a,
+        'miRNA_s': miRNA_s,
+        'miRNA_a': miRNA_a,
+        'miRNA_length': miRNA_length,
+        'miRNA_min': miRNA_min,
+        'miRNA_max': miRNA_max,
+        'structure': structure,
+        'homogeneity': homogeneity,
+        'miRBase_link': miRBase_link
+    }
