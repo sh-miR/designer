ceb8370fa1b3e79c508e40c1b326338d0a4c7336
Rogulski <rogulski@prmain.(none)>
2013-06-02 19:21:13 +0200

    I end and w8 for Martyna to approved it

--- utils.py
+++ utils.py
@@ -2,13 +2,10 @@
 import re
 import math
 
-
-seq_to_be_check = raw_input("Add sequence: ")
-
-
 def check_complementary(seq1,seq2):
     
     def check(seq1,seq2):
+        seq1,seq2 = seq1.lower(), seq2.lower()
         tran = { "a":"t", "t":"a", "u":"a", "c":"g", "g":"c"}
         seq2 = seq2[::-1]
         maks = float(max(len(seq1), len(seq2)))
@@ -37,58 +34,49 @@ def check_complementary(seq1,seq2):
     else:
         return (seq1,seq2,2)
 
-
 def check_input(seq_to_be_check):
     __pattern = re.compile(
         r'^[acgtACGT]{17,27}$') 
     error = 'insert only one siRNA sequence or both strands of one siRNA at a time; check if both stands are in 5-3 orientation'
     changed = "changed 'u' to 't'"
-
+    correct = "correct sequence"
     def check(seq):
         seq = seq.lower() 
-        if len(seq) > 27 or len(seq) < 17 :
-            message = [seq, "to long or to short"] 
-        
-        elif seq[-2:] == 'uu' or seq[-2:] == "tt":
+               
+        if seq[-2:] == 'uu' or seq[-2:] == "tt":
             seq = seq[:-2]
-            message = [seq,"cut 'uu' or 'tt'"]
+            message = [seq,"cut 'uu' or 'tt'"], True
+         
+        if len(seq) > 27 or len(seq) < 17 :
+            message = [seq, "to long or to short"], False 
 
         elif __pattern.search(seq):
-            message = [seq,"correct sequence"] 
+            message = [seq,correct], True 
 
         elif 'u' in seq:
             seq = seq.replace('u','t')
-            message = [seq,changed] 
+            message = [seq,changed],True 
 
+        elif not __pattern(seq):
+            message = [seq,"insert only acgtu letters"],False
         else:
-            message = [seq,"insert only acgtu letters"]
+            message = [seq,error],False
         return message
 
-    sequence = seq_to_be_check.split(" ")
-    print sequence
+    sequence = seq_to_be_check.split(" ") 
 
-#do naprawy to na dole w (niedziele) 
-    if len(sequence) == 2:
-        zm = [check(sequence[0]),check(sequence[1])]
-        print zm
-        if (zm[0][1] == zm[1][1] == "correct sequence") and (check_complementary(zm[0][0],zm[1][0])[2] == 1) :
-            return [zm[0][0],zm[1][0],"correct sequence"]
-        
-        elif (zm[0][1] == error or zm[1][1] == error) or ((zm[0][1] == zm[1][1] == "correct sequence") and (check_complementary(zm[0][0],zm[1][0])[2] == 2)):
-                return [zm[0][0],zm[1][0],error]
-
-        elif ( zm[0][1] == changed  or zm[1][1] == changed ) and (check_complementary(zm[0][0],zm[1][0])[2] == 1):
-            return [zm[0][0],zm[1][0],changed]
+    if len(sequence)==1:
+        return check(sequence[0])[0]
+    elif len(sequence)==2:
+        ch_seq1,ch_seq2 = check(sequence[0]), check(sequence[1])
+        if (ch_seq1[1],ch_seq2[1] is True,True) and check_complementary(sequence[0],sequence[1])[2]==1:
+            return [sequence[0],sequence[1],correct]
+        else:
+            return [ch_seq1[0],ch_seq2[0],"Not complementary"]
 
 
-    elif len(sequence) == 1:
-        return check(sequence[0])
-    else:
-        return error    
-        
-    
-    
 
-cos = check_complementary("tact","tga")
+#uncomment to check if its worikng coz tests are little diffrent and I have to w8 for Martyna to approve it <3
+#print check_input("acggctTggaacttctggtac")
 
-print check_input(seq_to_be_check)
+#print check_input('acggcttGGaacttctggtac gtaccagaagttccaagccgt')
