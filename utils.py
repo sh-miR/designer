07246af90703f6ccce7bd02822f19d6b5e99bf18
Rogulski <rogulski@prmain>
2013-07-23 21:51:18 +0200

    Take inside functions outside and upgrade

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ import re
 import math
 
 def check_complementary(seq1, seq2):
-    """Checking complementary function""" 
-    def check(seq1, seq2):
         seq1, seq2 = seq1.lower(), seq2.lower()
         tran = { "a":"t", 
                  "t":"a", 
@@ -19,40 +17,51 @@ def check_complementary(seq1, seq2):
                 count += 1
         proc = (count/mini)*100
         return math.floor(proc) 
-    
-    nr_offset = 3 # number of offset
+
+
+def check_offset_complementary(seq1, seq2):
+    """Checking complementary function""" 
+     
+    nr_offset = 3 
     for i in range(1, nr_offset):
     
-        max1,max2 = max(len(seq1[:-i]),len(seq2)),max(len(seq1),len(seq2[:-i]))
-        min1,min2 = min(len(seq1[:-i]),len(seq2)),min(len(seq1),len(seq2[:-i]))
+        max1, max2 = max(len(seq1[:-i]),len(seq2)),max(len(seq1),len(seq2[:-i]))
+        min1, min2 = min(len(seq1[:-i]),len(seq2)),min(len(seq1),len(seq2[:-i]))
+ 
+        if check_complementary(seq1[i:], seq2) >= 80:
+            return seq1[i:], seq2, 1
 
-        if (check(seq1[i:], seq2) or check(seq1, seq2[i:])) >= 80:
-            #'or' because we need only one 2-sequence which are complementary 
-            return (seq1, seq2, 1) #last string for me 
+        elif check_complementary(seq1, seq2[i:]) >= 80:
+            return seq1, seq2[i:], 1
 
-        elif (check(seq1[:-i], seq2[(max1-min1):]) or \
-              check(seq1[(max2-min2):],seq2[:-i])) >= 80: 
-            return (seq1, seq2, 1)
-#wprowadzic wypluwanie ktore przysuniete sekwencje
-        else:
-            return (seq1, seq2, 2)
+        elif check_complementary(seq1[:-i], seq2[(max1-min1):]) >= 80: 
+            return seq1[:-i], seq2[(max1-min1):], 1 
+
+        elif check_complementary(seq1[(max2-min2):], seq2[:-i]) >= 80:
+            return seq1[(max2-min2):], seq2[:-1], 1
+
+    if check_complementary(seq1, seq2) >= 80:
+        return seq1, seq2, 1
+
+    elif check_complementary(seq1[::-1], seq2[::-1]) >= 80:
+        return seq1[::-1], seq2[::-1], 1
+
+    else:
+        return seq1, seq2, 2
+
+
 #'insert only one siRNA sequence or both strands of one siRNA at a\
 #time; check if both stands are in 5-3 orientation'
  
-
-def check_input(seq_to_be_check):
-    """Checking input finction"""
-    pattern = re.compile(r'^[acgt]{17,27}$') 
-    error = 'insert only one siRNA sequence or both strands of one siRNA at a\
-time; check if both stands are in 5-3 orientation'
-    correct = "correct sequence"
-    
-    def check(seq):
-        """Checking error file"""
+def check_input(seq):
+        """Function for check sequence from input"""
         seq = seq.lower() 
         seq = seq.replace('u','t') 
-        
-        if len(seq) > 27 or len(seq) < 17 :
+        pattern = re.compile(r'^[acgt]{17,27}$') 
+        error = 'insert only one siRNA sequence or both strands of one \
+siRNA at a time; check if both stands are in 5-3 orientation'
+
+        if len(seq) > 27 or len(seq) < 17:
             return  [seq, "to long or to short", False]
 
         elif seq[-2:] == "tt":
@@ -60,21 +69,27 @@ time; check if both stands are in 5-3 orientation'
             return [seq, "cut 'uu' or 'tt'", True]
 
         elif pattern.search(seq):
-            return [seq, correct, True] 
+            return [seq, "correct sequence", True] 
 
         elif not pattern.search(seq):
             return [seq, "insert only acgtu letters", False]
 
-    sequence = seq_to_be_check.split(" ") 
+def check_many_inputs(seq_to_be_check):
+    """Function for checking many sequences and throw error if wrong input"""
 
+    correct = "correct sequence" 
+    sequence = seq_to_be_check.split(" ") 
+    error = 'insert only one siRNA sequence or both strands of one siRNA at a\
+time; check if both stands are in 5-3 orientation'
+    
     if len(sequence) == 1:
-        return check(sequence[0])[:2]
+        return check_input(sequence[0])[:2]
 
     elif len(sequence) == 2:
-        ch_seq1, ch_seq2 = check(sequence[2]), check(sequence[2])
+        ch_seq1, ch_seq2 = check_input(sequence[2]), check_input(sequence[2])
         
         if (ch_seq1[1], ch_seq2[1] is True, True) and \
-            check_complementary(sequence[0], sequence[1])[2] == 1:
+            check_offset_complementary(sequence[0], sequence[1])[2] == 1:
             return [sequence[0], sequence[1], correct]
 
     else:
@@ -82,13 +97,8 @@ time; check if both stands are in 5-3 orientation'
 
 
 
-#print check_input("acggctTggaacttctggtac")
 
-str1 = 'acggcttGGaacttctggtac' 
-str2 = 'gtaccagaagttccaagcc'
 
-str11='acggcttGGaacttctggtac'
-str22='gtaccagaagttccaagccgt'
 
-check_complementary(str11, str22)
+print check_offset_complementary('acggcttGGaacttctggtac', 'gtaccagaagttccaagccgta')
 
