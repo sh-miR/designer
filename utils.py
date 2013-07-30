4ccb74fb3c6e06d82b098e56150677807cd17a6f
Piotr Rogulski <piotr.rogulski@stxnext.pl>
2013-07-30 19:37:37 +0200

    Changed function names

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import re
 import math
 
-def check_complementary(seq1, seq2):
+def check_complementary_single(seq1, seq2):
         seq1, seq2 = seq1.lower(), seq2.lower()
         tran = { "a":"t", 
                  "t":"a", 
@@ -19,47 +19,47 @@ def check_complementary(seq1, seq2):
         return math.floor(proc) 
 
 
-def check_offset_complementary(seq1, seq2):
+def check_complementary(seq1, seq2):
     """Checking complementary function""" 
     
     nr_offset = 3 
     
-    if check_complementary(seq1, seq2) >= 80:
+    if check_complementary_single(seq1, seq2) >= 80:
         return seq1, seq2, 0, 1 
 
-    elif check_complementary(seq1[::-1], seq2[::-1]) >= 80:
+    elif check_complementary_single(seq1[::-1], seq2[::-1]) >= 80:
         return seq1[::-1], seq2[::-1], 0, 2  #<- Return reversed or normal?
 
     for i in range(1, nr_offset):
-        #1 - mean checked from beggin, 2 mean checked from the end
         
         max1, max2 = max(len(seq1[:-i]),len(seq2)),max(len(seq1),len(seq2[:-i]))
         min1, min2 = min(len(seq1[:-i]),len(seq2)),min(len(seq1),len(seq2[:-i]))
  
-        if check_complementary(seq1[i:], seq2) >= 80:
-            return seq1[i:], seq2, -i, 1 
+        if check_complementary_single(seq1[i:], seq2) >= 80:
+            return seq1[i:], seq2, i, 1 
 
-        elif check_complementary(seq1, seq2[i:]) >= 80:
-            return seq1, seq2[i:], i, 1
+        elif check_complementary_single(seq1, seq2[i:]) >= 80:
+            return seq1, seq2[i:], -i, 1 
 
-        elif check_complementary(seq1[:-i], seq2[(max1-min1):]) >= 80: 
-            return seq1[:-i], seq2[(max1-min1):], -i, 2
+        elif check_complementary_single(seq1[:-i], seq2[(max1-min1):]) >= 80: 
+            return seq1[:-i], seq2[(max1-min1):], i, 2
 
-        elif check_complementary(seq1[(max2-min2):], seq2[:-i]) >= 80:
-            return seq1[(max2-min2):], seq2[:-1], i, 2 
+        elif check_complementary_single(seq1[(max2-min2):], seq2[:-i]) >= 80:
+            return seq1[(max2-min2):], seq2[:-1], -i, 2 
 
     else:
         return seq1, seq2, 'insert only one siRNA sequence or both strands of \
 one siRNA at a time; check if both stands are in 5-3 orientation'
 
 
-def check_input(seq):
+def check_input_single(seq):
         """Function for check sequence from input"""
         seq = seq.lower() 
         seq = seq.replace('u','t') 
         pattern = re.compile(r'^[acgt]{17,27}$') 
         error = 'insert only one siRNA sequence or both strands of one \
 siRNA at a time; check if both stands are in 5-3 orientation'
+
 
         if len(seq) > 27 or len(seq) < 17:
             return  [seq, "to long or to short", False]
@@ -74,7 +74,7 @@ siRNA at a time; check if both stands are in 5-3 orientation'
         elif not pattern.search(seq):
             return [seq, "insert only acgtu letters", False]
 
-def check_many_inputs(seq_to_be_check):
+def check_input(seq_to_be_check):
     """Function for checking many sequences and throw error if wrong input"""
 
     correct = "correct sequence" 
@@ -86,13 +86,12 @@ time; check if both stands are in 5-3 orientation'
         return check_input(sequence[0])[:2]
 
     elif len(sequence) == 2:
-        ch_seq1, ch_seq2 = check_input(sequence[0]), check_input(sequence[1])
+        ch_seq1, ch_seq2 = check_input_single(sequence[0]), check_input_single(sequence[1])
         
         if (ch_seq1[1], ch_seq2[1] is True, True) and \
-            check_offset_complementary(sequence[0], sequence[1])[2] == 1:
+            check_complementary(sequence[0], sequence[1])[2] == 1:
             return [sequence[0], sequence[1], correct]
 
     else:
         return error 
 
-print check_offset_complementary('acggcttGGaacttctggtac', 'gaaggtgaagttccaagccgt')
