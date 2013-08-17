1119b01b6cb8346a88403a09021ca6b115906379
Piotr <rivinek@gmail.com>
2013-08-17 18:13:04 +0200

    Added parser_ss file with function to parse ss

---
+++ parser_ss.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+import os
+os.chdir('structures')
+
+def parser_ss(file_name):
+    """Function for parse ss file and gives tuples with first
+        and last column on output"""
+    read_data = []
+    with open(file_name, "r") as ss_file:
+        for line in ss_file.readlines():
+            splited = line.split()
+            read_data.append((splited[0], splited[-1]))
+    return read_data
