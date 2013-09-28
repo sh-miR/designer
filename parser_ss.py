d2c86ed4ea412004b6ec5037c9df165cd6c36cf7
Mateusz Flieger <mateuszflieger@gmail.com>
2013-09-28 11:10:17 +0200

    From ss to normal function

--- parser_ss.py
+++ parser_ss.py
@@ -1,5 +1,3 @@
-#!usr/bin/env python
-
 def parser_ss(file_name):
     """Function for parse ss file and gives tuples with first
         and last column on output"""
@@ -9,3 +7,9 @@ def parser_ss(file_name):
             splited = line.split()
             read_data.append((splited[0], splited[-1]))
     return read_data
+
+def ss_to_normal():
+	for name in ('miR-122', 'miR-155', 'miR-21', 'miR-30a', 'miR-31'):
+		new_file = open(name, "w")
+		for elem in parser_ss(name + ".ss"):
+		    new_file.write(" ".join(elem)+"\n")
