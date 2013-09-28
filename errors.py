1fec83bd9b4405a333826f3b81135f6a1a279d8c
Piotr <rivinek@gmail.com>
2013-09-29 01:45:32 +0200

    Forget add file

---
+++ errors.py
@@ -0,0 +1,11 @@
+#usr/bin/env python
+import logging
+
+
+class InputException(Exception):
+    """Exception error class for incorrect input"""
+
+    def __init__(self, message=None):
+        self.message = message
+        #logging.error('%s: %s' % [self.__class__.__name__, self.message])
+        super(InputException, self).__init__(self.message)
