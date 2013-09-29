63520484b19a1cbd773f2da7eb153d630731eb5f
Piotr <rivinek@gmail.com>
2013-09-29 11:08:06 +0200

    [#33] Added logging errors

--- errors.py
+++ errors.py
@@ -7,5 +7,5 @@ class InputException(Exception):
 
     def __init__(self, message=None):
         self.message = message
-        #logging.error('%s: %s' % [self.__class__.__name__, self.message])
+        logging.error('%s: %s', self.__class__.__name__, self.message)
         super(InputException, self).__init__(self.message)
