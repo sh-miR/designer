1c87f6f6ca04695ddbf41bcf8df571f0c7c10214
Mateusz Flieger <mateuszflieger@gmail.com>
2013-10-01 23:32:43 +0200

    Mfold

--- mfold.py
+++ mfold.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+from zipfile import ZipFile
 
 URL = 'http://127.0.0.1:5000/mfold'
 
@@ -9,12 +10,13 @@ HEADERS = {'content-type': 'application/json'}
 def mfold(data=None):
     json_data = {'data': data}
 
-    #import pdb
-    #pdb.set_trace()
-
     req = urllib2.Request(URL, json.dumps(json_data), HEADERS)
 
-    try:
-        return json.loads(urllib2.urlopen(req).read())
-    except urllib2.URLError:
-        return {'error': 'Connection to database refused'}
+    with open("dat.zip", "wb") as f:
+        f.write(urllib2.urlopen(req).read())
+
+
+    # try:
+    #     return json.loads(urllib2.urlopen(req).read())
+    # except urllib2.URLError:
+    #     return {'error': 'Connection to database refused'}
