9ee99bfcb4e5d4b6c80122cd5f6dde124c278fe8
Mateusz Flieger <mateuszflieger@gmail.com>
2014-03-25 11:38:17 +0100

    #51 findall_overlapping and changed patterns to dict

--- shmir_designer/search.py
+++ shmir_designer/search.py
@@ -6,15 +6,36 @@
 import re
 
 
+def findall_overlapping(pattern, string):
+    """
+    :param pattern: Pattern string.
+    :type pattern: str.
+    :param string: String in which we search.param
+    :type string: str.
+    :returns: list -- all overlapping matches of pattern in string.
+    """
+    regex = re.compile(pattern)
+    results = []
+    pos = 0
+
+    while True:
+        result = regex.search(string, pos)
+        if not result:
+            break
+        results.append(result.group(0))
+        pos = result.start() + 1
+    return results
+
+
 def find_by_patterns(patterns, mRNA):
     """This function search for patterns in mRNA
-    :param patterns: List of patterns
-    :type patterns: list.
-    :param mRNA: mRNA sequnece
+    :param patterns: Dict of patterns.
+    :type patterns: dict.
+    :param mRNA: mRNA sequnece.
     :type mRNA: str.
-    :returns: list -- all sequences found by patterns
+    :returns: dict -- all sequences found by patterns.
     """
-    sequneces = []
-    for pattern in patterns:  # will be changed if not list
-        sequneces += re.findall(pattern, mRNA)
-    return sequneces
+    return {
+        key: findall_overlapping(pattern, mRNA)
+        for key, pattern in patterns.items()
+    }
