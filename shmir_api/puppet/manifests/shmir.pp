e73f7e6abca7c1b4af6e960430f0827a3c7e9107
Michał Rostecki <nihilifer@nihilifer.net>
2014-04-08 20:05:41 +0200

    vagrant with centos & py27

--- shmir_api/puppet/manifests/shmir.pp
+++ shmir_api/puppet/manifests/shmir.pp
@@ -7,3 +7,17 @@ supervisor::service {
     user        => 'vagrant',
     group       => 'vagrant',
 }
+
+package {
+  'ius-release':
+    ensure      => installed,
+    source      => "http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-11.ius.centos6.noarch.rpm",
+    provider    => rpm
+}
+
+$packages = [ "python27", "python27-devel" ]
+package {
+  $packages:
+    ensure      => installed,
+    require     => Package["ius-release"]
+}
