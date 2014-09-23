234f6409f0bda6168061a285783c38104e3a9a05
Mateusz Flieger <mateuszflieger@gmail.com>
2014-09-23 18:28:30 +0200

    refactor offtarget

--- src/shmir/designer/offtarget.py
+++ src/shmir/designer/offtarget.py
@@ -2,20 +2,24 @@ from Bio.Blast import NCBIWWW, NCBIXML
 import cStringIO
 
 
-
 def blast_offtarget(fasta_string):
-    result_handle = NCBIWWW.qblast("blastn", "refseq_rna", fasta_string, entrez_query="txid9606 [ORGN]", expect=100, gapcosts="5 2", genetic_code=1, hitlist_size=100, word_size=len(fasta_string), megablast=True)
+    result_handle = NCBIWWW.qblast(
+        "blastn", "refseq_rna", fasta_string, entrez_query="txid9606 [ORGN]",
+        expect=100, gapcosts="5 2", genetic_code=1, hitlist_size=100,
+        word_size=len(fasta_string), megablast=True
+    )
     blast_results = result_handle.read()
     blast_in = cStringIO.StringIO(blast_results)
     count = 0
     for record in NCBIXML.parse(blast_in):
-        for align in record.alignments :
+        for align in record.alignments:
             count = count + 1
     return count
-    
-def check_offtarget(min, max, fasta_string):
-    actual_offtarget=blast_offtarget(fasta_string)
-    if (actual_offtarget<=max and actual_offtarget>=min):
+
+
+def check_offtarget(min_offtarget, max_offtarget, fasta_string):
+    actual_offtarget = blast_offtarget(fasta_string)
+    if actual_offtarget <= max_offtarget and actual_offtarget >= min_offtarget:
         return 0
     else:
         return 1
